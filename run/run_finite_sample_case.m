dc59669077f05a2bd77ce58f8186dee75be72e0f
ankani <ankanichattoraj91@gmail.com>
2018-10-01 15:02:19 -0400

    Included fig 2 plot from NIPS paper

--- run/run_finite_sample_case.m
+++ run/run_finite_sample_case.m
@@ -3,21 +3,22 @@
 
 % close all
 % clear all
-N = 101; %Number of neurons
+N = 21; %Number of neurons
 stim_num = N; %Number of stimulus = number of neurons
 sig_eb = 0.1; %In NIPS paper \sigma_{exp-brain}
 pixel_noise_std = 0.5; %Pixel Noise of images (gratings)
 prior = 0.1; %Sparse prior of spiking of neurons
+ang_val = pi/2;
 [G,pix] = tools.PFgenerator(N,-ang_val,ang_val*(N-1)/N); %generates PFs
 params = tools.ModelParams(G,N,pixel_noise_std,prior,pix); %assigns parameters for inference
-ang_val = pi/2;
-Im = tools.StimGenerator(params.n_neurons,-ang_val,ang_val*(params.n_neurons-1)/params.n_neurons);
+
+Im = tools.StimGenerator(params.n_neurons,-1*ang_val,ang_val*(params.n_neurons-1)/params.n_neurons);
 fix_item = fix(params.n_neurons/2)+1; %Image whose orientation is to be determined
 Im_selected = Im(:,fix_item);
 
 %Drawing finite independent samples
 thin_gap = 50;
-n_samples = 2;% number of effective samples needed
+n_samples = 2000;% number of effective samples needed
 n_total_samples = thin_gap*n_samples;
 
 samples = tools.InferenceGibbs(params, Im_selected, n_total_samples, 1);
